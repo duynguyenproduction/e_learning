07eba7710183578f6d25dd1d107ba13e23fead04
duynguyena3win <duynguyena3win@gmail.com>
2014-12-16 22:58:27 +0700

    + Add Webservice ASP.NET Web Client ! Update Website E-Learning

---
+++ Website_E_Learning/Controllers/CoursesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Website_E_Learning.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Course for User!";
+            return View();
+        }
+
+        public ActionResult ShowCourse()
+        {
+            return View();
+        }
+    }
+}
